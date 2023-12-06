db2437831d83a251bbd76a7156221655237e664e
Tristan <trs.nvz@outlook.fr>
2023-12-06 17:09:16 +0100

    ADD Hero color title split

--- src/components/Hero/Hero5.jsx
+++ src/components/Hero/Hero5.jsx
@@ -5,35 +5,37 @@ import Div from '../Div';
 import VerticalLinks from '../VerticalLinks';
 
 export default function Hero5({
-  title,
-  subtitle,
-  btnLink,
-  btnText,
-  socialLinksHeading,
-  heroSocialLinks,
-}) {
+                                title,
+                                subtitle,
+                                btnLink,
+                                btnText,
+                                socialLinksHeading,
+                                heroSocialLinks,
+                              }) {
+  const titleParts = title.split(' ');
+  const leftPart = titleParts.slice(0, titleParts.length - 1).join(' ');
+  const rightPart = titleParts[titleParts.length - 1];
+
   return (
-    <Div
-      className="cs-hero cs-style3 cs-bg cs-fixed_bg cs-shape_wrap_1"
-      id="home"
-    >
-      <Div className="cs-shape_1" />
-      <Div className="cs-shape_1" />
-      <Div className="cs-shape_1" />
-      <Div className="cs-circle_1" />
-      <Div className="cs-circle_2" />
-      <Div className="container">
-        <Div className="cs-hero_text">
-          <h1 className="cs-hero_title text-end">{parse(title)}</h1>
-          <Div className="cs-hero_subtitle text-end">{subtitle}</Div>
-          <Button btnLink={btnLink} btnText={btnText} />
+      <Div className="cs-hero cs-style3 cs-bg cs-fixed_bg cs-shape_wrap_1" id="home">
+        <Div className="cs-hero_logo">
+          <img src="/images/logo_flamme.png" alt="Logo Flamme" />
         </Div>
+        <Div className="container">
+          <Div className="cs-hero_text">
+            <h1 className="cs-hero_title text-end">
+              <span style={{ color: 'white' }}>{leftPart} </span>
+              <span style={{ color: 'orange' }}>{rightPart}</span>
+            </h1>
+            <Div className="cs-hero_subtitle text-end">{subtitle}</Div>
+            <Button btnLink={btnLink} btnText={btnText} />
+          </Div>
+        </Div>
+        <VerticalLinks
+            data={heroSocialLinks}
+            title={socialLinksHeading}
+            variant="cs-left_side"
+        />
       </Div>
-      <VerticalLinks
-        data={heroSocialLinks}
-        title={socialLinksHeading}
-        variant="cs-left_side"
-      />
-    </Div>
   );
 }
