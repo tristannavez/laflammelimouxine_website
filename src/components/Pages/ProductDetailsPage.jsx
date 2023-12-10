8bff0db134e4341c3219b12f1cac112224172ab7
Tristan <trs.nvz@outlook.fr>
2023-12-10 17:42:27 +0100

    Update style ProductDetailsPage.jsx

--- src/components/Pages/ProductDetailsPage.jsx
+++ src/components/Pages/ProductDetailsPage.jsx
@@ -44,45 +44,46 @@ export default function ProductDetailsPage() {
             <Div className="container">
                 {productDetails && (
                     <>
-                        <img
-                            src={`${strapiUrl}${productDetails.attributes.image.data.attributes.formats.thumbnail.url}`}
-                            alt="Product Details"
-                            className="cs-radius_15 w-100"
-                        />
-                        <Spacing lg='90' md='40' />
-
                         <Div className="row">
+                            <Div className="col-lg-6">
+                                <img
+                                    src={`${strapiUrl}${productDetails.attributes.image.data.attributes.formats.thumbnail.url}`}
+                                    alt="Product Details"
+                                    className="cs-radius_15 w-100"
+                                />
+                            </Div>
+
                             <Div className="col-lg-6">
                                 <SectionHeading
                                     title={productDetails.attributes.title}
                                     subtitle={productDetails.attributes.subtitle}
-                                >
-                                    <Spacing lg='40' md='20' />
-                                    <p>{productDetails.attributes.description}</p>
-                                </SectionHeading>
-                            </Div>
-
-                            <Div className="col-lg-5 offset-lg-1">
-                                <Spacing lg='60' md='40' />
-                                <h2 className='cs-font_30 cs-font_26_sm cs-m0'>Informations du produit -</h2>
-                                <Spacing lg='50' md='30' />
-
-                                <Div className="row">
-                                    <Div className="col-6">
-                                        <h3 className='cs-accent_color cs-font_22 cs-font_18_sm cs-m0'>Catégorie(s):</h3>
-                                        <p className='cs-m0'>
-                                            {productDetails.attributes.categories?.data
-                                                ?.map(category => category.attributes.name)
-                                                .join(', ')}
-                                        </p>
-                                        <Spacing lg='30' md='30' />
-                                    </Div>
-                                    {/* Ajoutez d'autres détails du produit en fonction de votre structure de données */}
-                                </Div>
+                                />
+                                <Spacing lg='40' md='20' />
+                                <p>{productDetails.attributes.description}</p>
                             </Div>
                         </Div>
 
-                        <Spacing lg='65' md='10' />
+                        <Spacing lg='40' md='20' />
+
+                        <Div className="row">
+                            <Div className="col-lg-6">
+                                <h2 className='cs-font_30 cs-font_26_sm cs-m0'>Informations du produit -</h2>
+                                <Spacing lg='20' md='10' />
+
+                                <Div>
+                                    <h3 className='cs-accent_color cs-font_22 cs-font_18_sm cs-m0'>Catégorie(s):</h3>
+                                    <p className='cs-m0'>
+                                        {productDetails.attributes.categories?.data
+                                            ?.map(category => category.attributes.name)
+                                            .join(', ')}
+                                    </p>
+                                </Div>
+
+                                {/* Ajoutez d'autres détails du produit en fonction de votre structure de données */}
+                            </Div>
+                        </Div>
+
+                        <Spacing lg='40' md='20' />
 
                     </>
                 )}
