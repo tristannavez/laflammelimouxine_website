5c5c77bea31c27b7f6131cae4caa7fde8b87f62c
Tristan <trs.nvz@outlook.fr>
2023-12-10 18:29:53 +0100

    Init BlogPage.jsx and fetch data from scrapi

--- src/components/Pages/BlogPage.jsx
+++ src/components/Pages/BlogPage.jsx
@@ -1,92 +1,68 @@
-import React, { useEffect } from 'react'
-import { pageTitle } from '../../helper'
-import Cta from '../Cta'
-import PageHeading from '../PageHeading'
-import Pagination from '../Pagination'
-import PostStyle2 from '../Post/PostStyle2'
-import Div from '../Div'
-import Sidebar from '../Sidebar.jsx'
-import Spacing from '../Spacing'
+import React, { useEffect, useState } from 'react';
+import { pageTitle } from '../../helper';
+import PageHeading from '../PageHeading';
+import Pagination from '../Pagination';
+import PostStyle2 from '../Post/PostStyle2';
+import Div from '../Div';
+import Spacing from '../Spacing';
+import config from '../../config/config';
 
 export default function BlogPage() {
-  pageTitle('Actualités');
-  const postData = [
-    {
-      thumb:'/images/post_4.jpeg',
-      title:'A.I will take all human job within next year',
-      subtitle:'Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Tortor posuere ac ut consequat semper viverra nam libero justo. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Purus sit amet luctus venenatis lectus. Nunc aliquet bibendum enim facilisis. Pretium viverra suspendisse potenti nullam ac tortor vitae.',
-      date:'07 Mar 2022',
-      category:'Tech',
-      categoryHref:'/blog',
-      href:'/blog/blog-details'
-    },
-    {
-      thumb:'/images/post_5.jpeg',
-      title:'Creative studio programm coming soon',
-      subtitle:'Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Tortor posuere ac ut consequat semper viverra nam libero justo. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Purus sit amet luctus venenatis lectus. Nunc aliquet bibendum enim facilisis. Pretium viverra suspendisse potenti nullam ac tortor vitae.',
-      date:'05 Mar 2022',
-      category:'Photography',
-      categoryHref:'/blog',
-      href:'/blog/blog-details'
-    },
-    {
-      thumb:'/images/post_6.jpeg',
-      title:'Artistic mind will be great for creation',
-      subtitle:'Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Tortor posuere ac ut consequat semper viverra nam libero justo. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Purus sit amet luctus venenatis lectus. Nunc aliquet bibendum enim facilisis. Pretium viverra suspendisse potenti nullam ac tortor vitae.',
-      date:'04 Mar 2022',
-      category:'Tech',
-      categoryHref:'/blog',
-      href:'/blog/blog-details'
-    }
-  ]
+    pageTitle('Actualités');
+    const [postData, setPostData] = useState([]);
+    const strapiUrl = config.strapiUrl;
 
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-  
-  return (
-    <>
-      <PageHeading 
-        title='Our Blog'
-        bgSrc='/images/blog_hero_bg.jpeg'
-        pageLinkText='Blog'
-      />
-      <Spacing lg='150' md='80'/>
-      <Div className="container">
-        <Div className="row">
-          <Div className="col-lg-8">
-            {postData.map((item, index)=> (
-              <Div key={index}>
-                <PostStyle2 
-                  thumb={item.thumb}
-                  title={item.title}
-                  subtitle={item.subtitle}
-                  date={item.date}
-                  category={item.category}
-                  categoryHref={item.categoryHref}
-                  href={item.href}
-                />
-                {postData.length>index+1 && <Spacing lg='95' md='60'/>}
-              </Div>
-            ))}
-            <Spacing lg='60' md='40'/>
-            <Pagination/>
-          </Div>
-          <Div className="col-xl-3 col-lg-4 offset-xl-1">
-            <Spacing lg='0' md='80'/>
-            <Sidebar/>
-          </Div>
-        </Div>
-      </Div>
-      <Spacing lg='150' md='80'/>
-      <Div className="container">
-        <Cta 
-          title='Let’s disscuse make <br />something <i>cool</i> together' 
-          btnText='Apply For Meeting' 
-          btnLink='/contact' 
-          bgSrc='/images/cta_bg.jpeg'
-        />
-      </Div>
-    </>
-  )
+    useEffect(() => {
+        const fetchBlogPosts = async () => {
+            try {
+                const response = await fetch(`${strapiUrl}/api/blog-posts?populate=*`);
+                const data = await response.json();
+
+                if (data && data.data) {
+                    setPostData(data.data);
+                } else {
+                    console.error('Invalid API response:', data);
+                }
+            } catch (error) {
+                console.error('Error fetching blog posts:', error);
+            }
+        };
+
+        fetchBlogPosts();
+        window.scrollTo(0, 0);
+    }, [strapiUrl]);
+
+    return (
+        <>
+            <PageHeading
+                title="Nos actualités"
+                bgSrc="/images/blog_hero_bg.jpeg"
+                pageLinkText="Actualités"
+            />
+            <Spacing lg="150" md="80" />
+            <Div className="container">
+                <Div className="row">
+                    <Div className="col-lg-12">
+                        {postData.map((item, index) => (
+                            <Div key={index}>
+                                <PostStyle2
+                                    title={item.attributes.title}
+                                    thumb={`${strapiUrl}${item.attributes.image.data.attributes.formats.thumbnail.url}`}
+                                    subtitle={item.attributes.subtitle}
+                                    date={item.attributes.date}
+                                    category={item.attributes.category}
+                                    categoryHref="/blog" // Remplacez par la bonne valeur
+                                    href="/blog/blog-details" // Remplacez par la bonne valeur
+                                />
+                                {postData.length > index + 1 && <Spacing lg="95" md="60" />}
+                            </Div>
+                        ))}
+                        <Spacing lg="60" md="40" />
+                        <Pagination />
+                    </Div>
+                </Div>
+            </Div>
+            <Spacing lg="150" md="80" />
+        </>
+    );
 }
